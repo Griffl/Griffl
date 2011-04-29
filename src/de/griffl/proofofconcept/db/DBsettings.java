48a18368efdad4f8c606520e17a8ca5b6b63415e
Sebastian Schneider <sebastian.schneider@uni-ulm.de>
2011-04-29 12:02:25 +0200

    Test comment added

--- src/de/griffl/proofofconcept/db/DBsettings.java
+++ src/de/griffl/proofofconcept/db/DBsettings.java
@@ -1,6 +1,7 @@
 package de.griffl.proofofconcept.db;
 
 public class DBsettings {
+	// Test
 	public static final String HOST = "localhost";
 	public static final int PORT = 5984;
 	public static final String DATABASE = "testgriffl";
